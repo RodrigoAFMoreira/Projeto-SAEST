682048a9bf7b734c1e63385e2046e19b0ff0235d
carolinesales <caroline.vsales@gmail.com>
2025-03-31 15:29:49 -0300

    implementação da estilização da página de Login

--- SAEST/js/firebase-config.js
+++ SAEST/js/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyD3Iuej48VfDT3KVF2m1-Gz3BSY5-bpvps",
-   authDomain: "saest-87e06.firebaseapp.com",
-   projectId: "saest-87e06",
-   storageBucket: "saest-87e06.firebasestorage.app",
-   messagingSenderId: "43278021134",
-   appId: "1:43278021134:web:6311f0e16924a3ae3dc25f",
-   measurementId: "G-W5E5NYVXF4"
+  apiKey: "AIzaSyD3Iuej48VfDT3KVF2m1-Gz3BSY5-bpvps",
+  authDomain: "saest-87e06.firebaseapp.com",
+  projectId: "saest-87e06",
+  storageBucket: "saest-87e06.firebasestorage.app",
+  messagingSenderId: "43278021134",
+  appId: "1:43278021134:web:6311f0e16924a3ae3dc25f",
+  measurementId: "G-W5E5NYVXF4"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,4 +18,4 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, analytics, auth, db };
+export { auth, db };
